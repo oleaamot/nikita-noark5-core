f63ec86d0026b8cfcc45ad6136b2dbb77e73c043
Thomas Sødring <tsodring@gmail.com>
2017-03-09 15:31:08 +0100

    Added proper support for documentMedium at controller/service/repository level
    DocumentMedium is one of many secondary entities that has to be implemented. A placeholder controller was in place, but this commit allows the user to interact with documentMedium properly.
    
     Service and Repository classes were introduced. Controller and import.sql files were updated. DocumentMedium was updated to include systemId

--- core-webapp/src/main/java/no/arkivlab/hioa/nikita/webapp/service/impl/metadata/DocumentMediumService.java
+++ core-webapp/src/main/java/no/arkivlab/hioa/nikita/webapp/service/impl/metadata/DocumentMediumService.java
@@ -6,6 +6,7 @@ import no.arkivlab.hioa.nikita.webapp.service.interfaces.metadata.IDocumentMediu
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,8 +33,11 @@ public class DocumentMediumService implements IDocumentMediumService {
      * @param documentMedium documentMedium object with values set
      * @return the newly persisted documentMedium object
      */
+    // TODO, probably check that the systemId is not already in use
     @Override
     public DocumentMedium createNewDocumentMedium(DocumentMedium documentMedium) {
+        documentMedium.setDeleted(false);
+        documentMedium.setOwnedBy(SecurityContextHolder.getContext().getAuthentication().getName());
         return documentMediumRepository.save(documentMedium);
     }
 
