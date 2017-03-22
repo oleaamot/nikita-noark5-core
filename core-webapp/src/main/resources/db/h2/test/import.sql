abd813bc2052a6ba3d1e2e3d4ca38650d513bb7a
Thomas Sødring <tsodring@gmail.com>
2017-03-22 16:05:54 +0100

    Update the login credentials to better reflect the values used by the Noark 5v4 interface standard

--- core-webapp/src/main/resources/db/h2/test/import.sql
+++ core-webapp/src/main/resources/db/h2/test/import.sql
@@ -1,25 +1,24 @@
-// Create Noark 5 core users for the test run
-
-insert into User (id, username, email, password_hash, account_non_locked, credentials_non_expired, account_non_expired, enabled, account_created_date) values (1, 'case', 'case_handler@example.com', 'password', true, true, true, true, '2016-08-08 00:00:00');
-insert into User (id, username, email, password_hash, account_non_locked, credentials_non_expired, account_non_expired, enabled, account_created_date) values (2, 'rm', 'rm@example.com', 'password', true, true, true, true, '2016-08-08 00:00:00');
+insert into User (id, username, email, password_hash, account_non_locked, credentials_non_expired, account_non_expired, enabled, account_created_date) values (1, 'casehandler', 'case_handler@example.com', 'password', true, true, true, true, '2016-08-08 00:00:00');
+insert into User (id, username, email, password_hash, account_non_locked, credentials_non_expired, account_non_expired, enabled, account_created_date) values (2, 'recordkeeper', 'rm@example.com', 'password', true, true, true, true, '2016-08-08 00:00:00');
 insert into User (id, username, email, password_hash, account_non_locked, credentials_non_expired, account_non_expired, enabled, account_created_date) values (3, 'admin', 'admin@example.com', 'password', true, true, true, true, '2016-08-08 00:00:00');
+insert into User (id, username, email, password_hash, account_non_locked, credentials_non_expired, account_non_expired, enabled, account_created_date) values (4, 'leader', 'leader@example.com', 'password', true, true, true, true, '2016-08-08 00:00:00');
+insert into User (id, username, email, password_hash, account_non_locked, credentials_non_expired, account_non_expired, enabled, account_created_date) values (5, 'guest', 'guest@example.com', 'password', true, true, true, true, '2016-08-08 00:00:00');
 
-// Create groups / (spring) authorities
-insert into Authority (name) values ('ADMIN');
-insert into Authority (name) values ('CASE_HANDLER');
-insert into Authority (name) values ('RECORDS_MANAGER');
+INSERT INTO Authority (name) VALUES ('RECORDS_MANAGER');
+INSERT INTO Authority (name) VALUES ('RECORDS_KEEPER');
+INSERT INTO Authority (name) VALUES ('CASE_HANDLER');
+INSERT INTO Authority (name) VALUES ('LEADER');
+INSERT INTO Authority (name) VALUES ('GUEST');
 
-// Assign users to a group
 insert into User_authority(user_id, authority_name) values (1, 'CASE_HANDLER');
-insert into User_authority(user_id, authority_name) values (2, 'RECORDS_MANAGER');
-insert into User_authority(user_id, authority_name) values (3, 'ADMIN');
+insert into User_authority(user_id, authority_name) values (2, 'RECORDS_KEEPER');
+insert into User_authority(user_id, authority_name) values (3, 'RECORDS_MANAGER');
+insert into User_authority(user_id, authority_name) values (4, 'LEADER');
+insert into User_authority(user_id, authority_name) values (5, 'GUEST');
 
-INSERT INTO document_medium (pk_document_medium_id, system_id, code, description, deleted)
-VALUES (0, '48e067ae-d794-4216-8a9d-7cb0ef03472c', 'Fysisk medium', 'Bare fysiske dokumenter', FALSE);
-INSERT INTO document_medium (pk_document_medium_id, system_id, code, description, deleted)
-VALUES (1, '1b617405-9dd6-4187-bee6-81ed4f4a0e1f', 'Elektronisk arkiv', 'Bare elektroniske dokumenter', FALSE);
-INSERT INTO document_medium (pk_document_medium_id, system_id, code, description, deleted)
-VALUES (2, '00d21eea-c231-4542-8e3a-964f441fbbd3', 'Blandet fysisk og elektronisk arkiv', '', FALSE);
+insert into document_medium (pk_document_medium_id, system_id, code, description, deleted) values (0, '48e067ae-d794-4216-8a9d-7cb0ef03472c', 'Fysisk medium', 'Bare fysiske dokumenter', false);
+insert into document_medium (pk_document_medium_id, system_id, code, description, deleted) values (1, '1b617405-9dd6-4187-bee6-81ed4f4a0e1f', 'Elektronisk arkiv', 'Bare elektroniske dokumenter', false);
+insert into document_medium (pk_document_medium_id, system_id, code, description, deleted) values (2, '00d21eea-c231-4542-8e3a-964f441fbbd3', 'Blandet fysisk og elektronisk arkiv', '', false);
 
-INSERT INTO fonds_status (pk_fonds_status_id, code, deleted) VALUES (0, 'Opprettet', FALSE);
-INSERT INTO fonds_status (pk_fonds_status_id, code, deleted) VALUES (1, 'Avsluttet', FALSE);
+insert into fonds_status(pk_fonds_status_id, code, deleted) values (0, 'Opprettet', false);
+insert into fonds_status(pk_fonds_status_id, code, deleted) values (1, 'Avsluttet', false);
