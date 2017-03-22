2dd2aee86bf075c2b1664a54de2a984eca9074c1
Alexander Alemayhu <alexander@alemayhu.com>
2017-03-22 19:53:11 +0100

    web: add endpoint variables
    Will be used by login page in follow up. This should
    be moved to a seperate file in the future.

--- web/javascripts/client.js
+++ web/javascripts/client.js
@@ -1,5 +1,11 @@
 let app = angular.module('nikita', []);
 
+// TODO: use endpoint class
+let base_url = "http://127.0.0.1:8092/noark5v4/";
+let login_url = base_url + "doLogin";
+let sign_up_url = base_url + "signup";
+
+
 let controller = app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
   $scope.app_version = "xyz";
   $http({
