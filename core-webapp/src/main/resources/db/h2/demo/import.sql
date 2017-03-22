6134c0971cba7db1d30efb0c62b03e87a43a1c0b
Thomas Sødring <tsodring@gmail.com>
2017-03-22 16:05:16 +0100

    Update the login credentials to better reflect the values used by the Noark 5v4 interface standard

--- core-webapp/src/main/resources/db/h2/demo/import.sql
+++ core-webapp/src/main/resources/db/h2/demo/import.sql
@@ -1,12 +1,20 @@
-insert into User (id, username, email, password_hash, account_non_locked, credentials_non_expired, account_non_expired, enabled, account_created_date) values (1, 'case', 'case_handler@example.com', 'password', true, true, true, true, '2016-08-08 00:00:00');
-insert into User (id, username, email, password_hash, account_non_locked, credentials_non_expired, account_non_expired, enabled, account_created_date) values (2, 'rm', 'rm@example.com', 'password', true, true, true, true, '2016-08-08 00:00:00');
+insert into User (id, username, email, password_hash, account_non_locked, credentials_non_expired, account_non_expired, enabled, account_created_date) values (1, 'casehandler', 'case_handler@example.com', 'password', true, true, true, true, '2016-08-08 00:00:00');
+insert into User (id, username, email, password_hash, account_non_locked, credentials_non_expired, account_non_expired, enabled, account_created_date) values (2, 'recordkeeper', 'rm@example.com', 'password', true, true, true, true, '2016-08-08 00:00:00');
 insert into User (id, username, email, password_hash, account_non_locked, credentials_non_expired, account_non_expired, enabled, account_created_date) values (3, 'admin', 'admin@example.com', 'password', true, true, true, true, '2016-08-08 00:00:00');
-INSERT INTO Authority (name) VALUES ('ADMIN');
-INSERT INTO Authority (name) VALUES ('CASE_HANDLER');
+insert into User (id, username, email, password_hash, account_non_locked, credentials_non_expired, account_non_expired, enabled, account_created_date) values (4, 'leader', 'leader@example.com', 'password', true, true, true, true, '2016-08-08 00:00:00');
+insert into User (id, username, email, password_hash, account_non_locked, credentials_non_expired, account_non_expired, enabled, account_created_date) values (5, 'guest', 'guest@example.com', 'password', true, true, true, true, '2016-08-08 00:00:00');
+
 INSERT INTO Authority (name) VALUES ('RECORDS_MANAGER');
+INSERT INTO Authority (name) VALUES ('RECORDS_KEEPER');
+INSERT INTO Authority (name) VALUES ('CASE_HANDLER');
+INSERT INTO Authority (name) VALUES ('LEADER');
+INSERT INTO Authority (name) VALUES ('GUEST');
+
 insert into User_authority(user_id, authority_name) values (1, 'CASE_HANDLER');
-insert into User_authority(user_id, authority_name) values (2, 'RECORDS_MANAGER');
-insert into User_authority(user_id, authority_name) values (3, 'ADMIN');
+insert into User_authority(user_id, authority_name) values (2, 'RECORDS_KEEPER');
+insert into User_authority(user_id, authority_name) values (3, 'RECORDS_MANAGER');
+insert into User_authority(user_id, authority_name) values (4, 'LEADER');
+insert into User_authority(user_id, authority_name) values (5, 'GUEST');
 
 insert into document_medium (pk_document_medium_id, system_id, code, description, deleted) values (0, '48e067ae-d794-4216-8a9d-7cb0ef03472c', 'Fysisk medium', 'Bare fysiske dokumenter', false);
 insert into document_medium (pk_document_medium_id, system_id, code, description, deleted) values (1, '1b617405-9dd6-4187-bee6-81ed4f4a0e1f', 'Elektronisk arkiv', 'Bare elektroniske dokumenter', false);
