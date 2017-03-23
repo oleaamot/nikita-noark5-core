d23f081da0a8a95393e19b0c88cfe9bc9ae11052
Thomas Sødring <tsodring@gmail.com>
2017-03-23 19:29:38 +0100

    Introduce PATHPattern to make mapping of URIs to roles easier to update.

---
+++ core-common/src/main/java/nikita/config/PATHPatterns.java
@@ -0,0 +1,10 @@
+package nikita.config;
+
+import static nikita.config.Constants.*;
+
+/**
+ * Created by tsodring on 3/22/17.
+ */
+public final class PATHPatterns {
+    public static final String PATTERN_NEW_FONDS_STRUCTURE_ALL = SLASH + HATEOAS_API_PATH + SLASH + NOARK_FONDS_STRUCTURE_PATH + SLASH + NEW + DASH + "*";
+}
