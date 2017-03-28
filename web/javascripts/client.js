a10e4677cc2e3dc5c5030eb3f6a2917f325f4760
Petter Reinholdtsen <pere@hungry.com>
2017-03-28 23:47:53 +0200

    Avoid hardcoding IPv4 address in client javascript, ues 'localhost' to increase the change for IPv6.

--- web/javascripts/client.js
+++ web/javascripts/client.js
@@ -1,7 +1,7 @@
 let app = angular.module('nikita', []);
 
 // TODO: use endpoint class
-let base_url = "http://127.0.0.1:8092/noark5v4/";
+let base_url = "http://localhost:8092/noark5v4/";
 let login_url = base_url + "doLogin";
 let sign_up_url = base_url + "signup";
 
