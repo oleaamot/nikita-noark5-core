4d293a487f05927700fb968d24608f264a825382
Thomas Sødring <tsodring@gmail.com>
2017-04-03 21:03:19 +0200

    The description field in document_medium import is set to '', which is not the same as null. This has been changed. Just remove description column in the insert statement.

--- core-webapp/src/main/resources/db/h2/dev/import.sql
+++ core-webapp/src/main/resources/db/h2/dev/import.sql
@@ -18,7 +18,7 @@ insert into nikita_user_authority(user_id, authority_id) values (5, 5);
 
 insert into document_medium (pk_document_medium_id, system_id, code, description, deleted, version) values (0, '48e067ae-d794-4216-8a9d-7cb0ef03472c', 'Fysisk medium', 'Bare fysiske dokumenter', false, 0);
 insert into document_medium (pk_document_medium_id, system_id, code, description, deleted, version) values (1, '1b617405-9dd6-4187-bee6-81ed4f4a0e1f', 'Elektronisk arkiv', 'Bare elektroniske dokumenter', false, 0);
-insert into document_medium (pk_document_medium_id, system_id, code, description, deleted, version) values (2, '00d21eea-c231-4542-8e3a-964f441fbbd3', 'Blandet fysisk og elektronisk arkiv', '', false, 0);
+insert into document_medium (pk_document_medium_id, system_id, code, deleted, version) values (2, '00d21eea-c231-4542-8e3a-964f441fbbd3', 'Blandet fysisk og elektronisk arkiv', false, 0);
 
 insert into fonds_status(pk_fonds_status_id, code, deleted, version) values (0, 'Opprettet', false, 0);
 insert into fonds_status(pk_fonds_status_id, code, deleted, version) values (1, 'Avsluttet', false, 0);
