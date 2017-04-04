a373b0a650f57215626c71b3b81c3d7fa4f9a7e4
Thomas Sødring <tsodring@gmail.com>
2017-04-04 22:24:28 +0200

    Remove commented out code. It's not possible postHandle the response and update it as the response has already been committed. Setting of etags is handled at the controller level instead. at the controller

--- core-webapp/src/main/java/no/arkivlab/hioa/nikita/webapp/web/interceptor/NikitaETAGInterceptor.java
+++ core-webapp/src/main/java/no/arkivlab/hioa/nikita/webapp/web/interceptor/NikitaETAGInterceptor.java
@@ -44,48 +44,4 @@ public class NikitaETAGInterceptor extends HandlerInterceptorAdapter {
         // Important! You must return true if all is OK or further filer processing stops
         return true;
     }
-
-    /**
-     * @param request
-     * @param response
-     * @param handler
-     * @param modelAndView
-     *                     <p>
-     *                     After a request has completed, make sure to add an eTag to requests that contain a single Noark entity.
-     *                     This is so that clients can update the object. We make no difference with respect to the request method.
-     *                     <p>
-     *                     If for some reason the NoarkEntity has a null value, then this code assumes that this is intentional
-     *                     and no eTag is generated. Without an eTag, the client cannot update the entity.
-     * @throws Exception
-     */
-    /*public void postHandle(
-            HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView)
-            throws Exception {
-
-        HandlerMethod handlerMethod = (HandlerMethod)handler;
-        MethodParameter parameter = handlerMethod.getReturnType();
-        parameter.get
-        // TODO : How are we handling empty results with this?
-        // If it's empty it won't be a  HateoasNoarkObject ... it's {} so the 2nd if should ignore it
-        if (handler instanceof HateoasNoarkObject) {
-            String superClassName = handler.getClass().getSuperclass().getName();
-            if (superClassName.equals(HateoasNoarkObject.class.getName())) {
-                HateoasNoarkObject outgoingObject = ((HateoasNoarkObject) handler);
-                if (outgoingObject.isSingleEntity()) {
-                    // There should only be a single entity in the list, so we will find the version for eTag
-                    // in the first entity. If there is more than one then something has gone wrong elsewhere
-                    // Don't see the need to check for everything.
-                    List entityList = outgoingObject.getList();
-                    INoarkSystemIdEntity noarkSystemIdEntity = (INoarkSystemIdEntity) entityList.get(0);
-                    if (noarkSystemIdEntity != null && noarkSystemIdEntity.getVersion() != null) {
-                        String version = noarkSystemIdEntity;
-                        response.addHeader(ETAG, version);
-                    } else {
-                        logger.warn("Unable to add etag as entity is null for request [" +
-                                request.getRequestURI() + "]");
-                    }
-                }
-            }
-        }
-    }*/
 }
