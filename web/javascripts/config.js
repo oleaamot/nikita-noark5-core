1ab5903f2d132df75615259524a18ac45ee67b8a
Petter Reinholdtsen <pere@hungry.com>
2017-05-02 23:06:03 +0200

    Remember config file.

---
+++ web/javascripts/config.js
@@ -0,0 +1,9 @@
+nikitaOptions = {
+    baseUrl: 'localhost:8092',
+    guiBaseUrl: 'http://localhost:3000',
+    protocol: 'http',
+    appName: 'noark5v4',
+    apiName: 'hateoas-api',
+    authPoint: 'auth',
+    enabled: true
+}
