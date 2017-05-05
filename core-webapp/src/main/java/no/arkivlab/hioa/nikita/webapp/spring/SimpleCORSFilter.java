830dbc6c889d742f86ec8a8a3154ccb9caa161fe
Thomas Sødring <tsodring@gmail.com>
2017-05-05 10:15:04 +0200

    Add Access-Control-Expose-Headers to the CORS filter.
    In the browser, it is not possible to see the Allows or ETAG headers.
    
    Information about this can be seen here:
    
    https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Expose-Headers
    
    We see the Allow/ETAG headers from curl, but not in the browser. This should
    fix that issue.

--- core-webapp/src/main/java/no/arkivlab/hioa/nikita/webapp/spring/SimpleCORSFilter.java
+++ core-webapp/src/main/java/no/arkivlab/hioa/nikita/webapp/spring/SimpleCORSFilter.java
@@ -33,7 +33,7 @@ public class SimpleCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Authorization, Origin");
-
+        response.setHeader("Access-Control-Expose-Headers", "Allow, ETAG");
         chain.doFilter(req, res);
     }
 
