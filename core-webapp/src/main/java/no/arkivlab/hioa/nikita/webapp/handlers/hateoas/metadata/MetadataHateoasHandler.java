859e6b585ee865e3fd7aeb9a7335f795e21b7752
Thomas Sødring <tsodring@gmail.com>
2017-05-14 19:01:05 +0200

    Remove the ny-* for metadata from the _links part of a GET of all elements of a particular entity. ny- is now only available of the metadata root. E.g they are only available here:
       http://localhost:8092/noark5v4/hateoas-api/metadata

--- core-webapp/src/main/java/no/arkivlab/hioa/nikita/webapp/handlers/hateoas/metadata/MetadataHateoasHandler.java
+++ core-webapp/src/main/java/no/arkivlab/hioa/nikita/webapp/handlers/hateoas/metadata/MetadataHateoasHandler.java
@@ -3,14 +3,9 @@ package no.arkivlab.hioa.nikita.webapp.handlers.hateoas.metadata;
 import nikita.model.noark5.v4.hateoas.IHateoasNoarkObject;
 import nikita.model.noark5.v4.hateoas.Link;
 import nikita.model.noark5.v4.interfaces.entities.INikitaEntity;
-import nikita.util.exceptions.NikitaException;
 import no.arkivlab.hioa.nikita.webapp.handlers.hateoas.HateoasHandler;
 import no.arkivlab.hioa.nikita.webapp.handlers.hateoas.interfaces.metadata.IMetadataHateoasHandler;
-import no.arkivlab.hioa.nikita.webapp.security.IAuthorisation;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 import static nikita.config.Constants.*;
 
@@ -21,27 +16,6 @@ import static nikita.config.Constants.*;
  */
 @Component()
 public class MetadataHateoasHandler extends HateoasHandler implements IMetadataHateoasHandler {
-
-    @Override
-    public void addLinks(IHateoasNoarkObject hateoasNoarkObject, HttpServletRequest request,
-                         IAuthorisation authorisation) {
-        super.addLinks(hateoasNoarkObject, request, authorisation);
-
-        // As this is a top-domain entity, we need to support ny-
-        // { "entity": [], "_links": [] }
-        if (!hateoasNoarkObject.isSingleEntity()) {
-            List<INikitaEntity> entities = hateoasNoarkObject.getList();
-            INikitaEntity entity = entities.get(0);
-            if (entity == null) {
-                throw new NikitaException("Internal error, could not create metadata new object");
-            }
-            String rel = NIKITA_CONFORMANCE_REL + NEW + DASH + entity.getBaseTypeName() + SLASH;
-            Link newCodeLink = new Link(contextPath + HATEOAS_API_PATH + SLASH +
-                    NOARK_METADATA_PATH + SLASH + NEW + DASH + entity.getBaseTypeName(),
-                    rel, false);
-            hateoasNoarkObject.addLink(newCodeLink);
-        }
-    }
 
     @Override
     public void addEntityLinks(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject) {
