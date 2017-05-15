bfa754677b85a2c9d16d8d1aacef62bf553a3f06
Thomas Sødring <tsodring@gmail.com>
2017-05-15 22:57:50 +0200

    Fix a bug where the parsing of Integer was resulting in the value being  set to 0. Not sure why it was happening, but it was a strange way to  parse the Integer anyway

--- core-common/src/main/java/nikita/util/deserialisers/DocumentObjectDeserializer.java
+++ core-common/src/main/java/nikita/util/deserialisers/DocumentObjectDeserializer.java
@@ -17,28 +17,28 @@ import static nikita.config.N5ResourceMappings.*;
 
 /**
  * Created by tsodring on 1/6/17.
- *
+ * <p>
  * Deserialise an incoming DocumentObject JSON object.
- *
+ * <p>
  * Having a own deserialiser is done to have more fine grained control over the input. This allows us to be less strict
  * with property names, allowing for both English and Norwegian property names
- *
- *
+ * <p>
+ * <p>
  * Note this implementation expects that the DocumentObject object to deserialise is in compliance with the Noark standard where
- * certain properties i.e. createdBy and createdDate are set by the core, not the caller. This deserializer will not 
+ * certain properties i.e. createdBy and createdDate are set by the core, not the caller. This deserializer will not
  * enforce this and will deserialize a documentObject object correctly. This is because e.g the import interface will require
  * such functionality.
- *
- *  - Testing of compliance of properties is handled by the core, either in DocumentObjectController or DocumentObjectService
- * 
- * Note. Currently we do not include 'id' or 'deleted' properties. 'id' is a primary key and it is assumed this is 
+ * <p>
+ * - Testing of compliance of properties is handled by the core, either in DocumentObjectController or DocumentObjectService
+ * <p>
+ * Note. Currently we do not include 'id' or 'deleted' properties. 'id' is a primary key and it is assumed this is
  * taken care of by the DBMS and 'deleted' is a field internal to the core to handle soft delete. Importing soft deleted
  * objects is something we do not consider necessary.
- *
+ * <p>
  * Note:
- *  - Unknown property values in the JSON will trigger an exception
- *  - Missing obligatory property values in the JSON will trigger an exception
- *  - DocumentObject has no obligatory values required to be present at instantiation time
+ * - Unknown property values in the JSON will trigger an exception
+ * - Missing obligatory property values in the JSON will trigger an exception
+ * - DocumentObject has no obligatory values required to be present at instantiation time
  */
 public class DocumentObjectDeserializer extends JsonDeserializer {
 
@@ -52,11 +52,11 @@ public class DocumentObjectDeserializer extends JsonDeserializer {
         ObjectNode objectNode = mapper.readTree(jsonParser);
 
         // Deserialise general DocumentObject properties
-        CommonUtils.Hateoas.Deserialize.deserialiseNoarkSystemIdEntity (documentObject, objectNode);
+        CommonUtils.Hateoas.Deserialize.deserialiseNoarkSystemIdEntity(documentObject, objectNode);
         // Deserialize versionNumber
         JsonNode currentNode = objectNode.get(DOCUMENT_OBJECT_VERSION_NUMBER);
         if (null != currentNode) {
-            documentObject.setVersionNumber(Integer.valueOf(currentNode.intValue()));
+            documentObject.setVersionNumber(new Integer(currentNode.intValue()));
             objectNode.remove(DOCUMENT_OBJECT_VERSION_NUMBER);
         }
         // Deserialize variantFormat
