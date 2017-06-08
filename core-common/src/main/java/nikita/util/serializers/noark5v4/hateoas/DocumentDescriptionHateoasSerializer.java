c4ce8b6599467d519c9a53619a0d1e6f119b3f07
Petter Reinholdtsen <pere@hungry.com>
2017-06-08 20:52:25 +0200

    Correct serialization of dokumentbeskrivelse.tilknyttetDato.
    It is date, not datetime.

--- core-common/src/main/java/nikita/util/serializers/noark5v4/hateoas/DocumentDescriptionHateoasSerializer.java
+++ core-common/src/main/java/nikita/util/serializers/noark5v4/hateoas/DocumentDescriptionHateoasSerializer.java
@@ -50,7 +50,7 @@ public class DocumentDescriptionHateoasSerializer extends HateoasSerializer impl
         CommonUtils.Hateoas.Serialize.printCreateEntity(jgen, documentDescription);
         if (documentDescription.getAssociationDate()!= null) {
             jgen.writeStringField(DOCUMENT_DESCRIPTION_ASSOCIATION_DATE,
-                    Serialize.formatDateTime(documentDescription.getAssociationDate()));
+                    Serialize.formatDate(documentDescription.getAssociationDate()));
         }
         if (documentDescription.getAssociatedWithRecordAs() != null) {
             jgen.writeStringField(DOCUMENT_DESCRIPTION_ASSOCIATED_WITH_RECORD_AS, documentDescription.getAssociatedWithRecordAs());
