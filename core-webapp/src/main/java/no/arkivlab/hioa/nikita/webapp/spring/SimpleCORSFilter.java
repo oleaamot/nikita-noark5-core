9a95d9b29ee07f40c965668551d1314022c20a3c
Thomas Sødring <tsodring@gmail.com>
2017-06-10 13:59:10 +0200

    Access-Control-Allow-Headers was missing ETAG, this became apparent when updating an object from the GUI

--- core-webapp/src/main/java/no/arkivlab/hioa/nikita/webapp/spring/SimpleCORSFilter.java
+++ core-webapp/src/main/java/no/arkivlab/hioa/nikita/webapp/spring/SimpleCORSFilter.java
@@ -40,7 +40,7 @@ public class SimpleCORSFilter implements Filter {
             response.setHeader("Access-Control-Allow-Credentials", "true");
             response.setHeader("Access-Control-Allow-Methods", Arrays.toString(allowMethods));
             response.setHeader("Access-Control-Max-Age", "3600");
-            response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Authorization, Origin");
+            response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Authorization, Origin, ETAG");
             response.setHeader("Access-Control-Expose-Headers", "Allow, ETAG");
         }
         chain.doFilter(req, res);
