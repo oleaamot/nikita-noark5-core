4fb51b1778bfcc6d80de1277c542d3016ef89b63
Thomas Sødring <tsodring@gmail.com>
2018-01-25 12:55:10 +0100

    Tidy up in ServiceService.
    We are slowly removing the spring-data approach to retrieving entities
    from the database. This commit removes some non-used code but also
    removes som bugs that Coverity is reporting. These bugs are in non-used
    code so it just makes sense to remove the offending code.

--- core-webapp/src/main/java/no/arkivlab/hioa/nikita/webapp/service/interfaces/IDocumentDescriptionService.java
+++ core-webapp/src/main/java/no/arkivlab/hioa/nikita/webapp/service/interfaces/IDocumentDescriptionService.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 import javax.validation.constraints.NotNull;
-import java.util.Date;
 import java.util.List;
 
 public interface IDocumentDescriptionService {
@@ -31,62 +30,7 @@ public interface IDocumentDescriptionService {
 	DocumentDescription findById(Long id);
 
 	// systemId
-    DocumentDescription findBySystemIdOrderBySystemId(String systemId);
-
-	// title
-	List<DocumentDescription> findByTitleAndOwnedBy(String title, String ownedBy);
-	List<DocumentDescription> findByTitleContainingAndOwnedBy(String title, String ownedBy);
-	List<DocumentDescription> findByTitleIgnoreCaseContainingAndOwnedBy(String title, String ownedBy);
-	List<DocumentDescription> findByTitleAndOwnedBy(String title, String ownedBy, Sort sort);
-	List<DocumentDescription> findByTitleContainingAndOwnedBy(String title, String ownedBy, Sort sort);
-	List<DocumentDescription> findByTitleIgnoreCaseContainingAndOwnedBy(String title, String ownedBy, Sort sort);
-
-	Page<DocumentDescription> findByTitleAndOwnedBy(String title, String ownedBy, Pageable pageable);
-	Page<DocumentDescription> findByTitleContainingAndOwnedBy(String title, String ownedBy, Pageable pageable);
-	Page<DocumentDescription> findByTitleIgnoreCaseContainingAndOwnedBy(String title, String ownedBy, Pageable pageable);
-
-	// description
-	List<DocumentDescription> findByDescriptionAndOwnedBy(String description, String ownedBy);
-	List<DocumentDescription> findByDescriptionContainingAndOwnedBy(String description, String ownedBy);
-	List<DocumentDescription> findByDescriptionIgnoreCaseContainingAndOwnedBy(String description, String ownedBy);
-	List<DocumentDescription> findByDescriptionAndOwnedBy(String description, String ownedBy, Sort sort);
-	List<DocumentDescription> findByDescriptionContainingAndOwnedBy(String description, String ownedBy, Sort sort);
-	List<DocumentDescription> findByDescriptionIgnoreCaseContainingAndOwnedBy(String description, String ownedBy, Sort sort);
-
-	Page<DocumentDescription> findByDescriptionAndOwnedBy(String description, String ownedBy, Pageable pageable);
-	Page<DocumentDescription> findByDescriptionContainingAndOwnedBy(String description, String ownedBy, Pageable pageable);
-	Page<DocumentDescription> findByDescriptionIgnoreCaseContainingAndOwnedBy(String description, String ownedBy, Pageable pageable);
-
-	// documentMedium
-	List<DocumentDescription> findByDocumentMediumAndOwnedBy(String documentMedium, String ownedBy);
-	List<DocumentDescription> findByDocumentMediumAndOwnedBy(String documentMedium, String ownedBy, Sort sort);
-	Page<DocumentDescription> findByDocumentMediumAndOwnedBy(String documentMedium, String ownedBy, Pageable pageable);
-
-	// createdDate
-	List<DocumentDescription> findByCreatedDateAndOwnedBy(Date createdDate, String ownedBy);
-	List<DocumentDescription> findByCreatedDateAndOwnedBy(Date createdDate, String ownedBy, Sort sort);
-	List<DocumentDescription> findByCreatedDateBetweenAndOwnedBy(Date start, Date end, String ownedBy);
-
-	Page<DocumentDescription> findByCreatedDateAndOwnedBy(Date createdDate, String ownedBy, Pageable pageable);
-	Page<DocumentDescription> findByCreatedDateBetweenAndOwnedBy(Date start, Date end, String ownedBy, Pageable pageable);
-
-	// createdBy
-	List<DocumentDescription> findByCreatedByAndOwnedBy(String createdBy, String ownedBy);
-	List<DocumentDescription> findByCreatedByContainingAndOwnedBy(String createdBy, String ownedBy);
-	List<DocumentDescription> findByCreatedByIgnoreCaseContainingAndOwnedBy(String createdBy, String ownedBy);
-	List<DocumentDescription> findByCreatedByAndOwnedBy(String createdBy, String ownedBy, Sort sort);
-	List<DocumentDescription> findByCreatedByContainingAndOwnedBy(String createdBy, String ownedBy, Sort sort);
-	List<DocumentDescription> findByCreatedByIgnoreCaseContainingAndOwnedBy(String createdBy, String ownedBy, Sort sort);
-
-	Page<DocumentDescription> findByCreatedByAndOwnedBy(String createdBy, String ownedBy, Pageable pageable);
-	Page<DocumentDescription> findByCreatedByContainingAndOwnedBy(String createdBy, String ownedBy, Pageable pageable);
-	Page<DocumentDescription> findByCreatedByIgnoreCaseContainingAndOwnedBy(String createdBy, String ownedBy, Pageable pageable);
-
-
-	// deleted
-	List<DocumentDescription> findByDeletedAndOwnedBy(String deleted, String ownedBy);
-	List<DocumentDescription> findByDeletedAndOwnedBy(String deleted, String ownedBy, Sort sort);
-	Page<DocumentDescription> findByDeletedAndOwnedBy(String deleted, String ownedBy, Pageable pageable);
+	DocumentDescription findBySystemId(String systemId);
 
 	// ownedBy
 	List<DocumentDescription> findByOwnedBy(String ownedBy);
