cf70ea63a38085a71701ec64ce2c2a6ff46dae07
Thomas Sødring <tsodring@gmail.com>
2018-03-06 08:21:36 +0100

    Definition of OData constants.
    Somehow the previous commit used a wrong version of this class. I
    probably just copied ESConstants and changed it but managed to commit
    the earlier version. This broke the build. This commit fixes the build.

--- core-common/src/main/java/nikita/config/ODataConstants.java
+++ core-common/src/main/java/nikita/config/ODataConstants.java
@@ -1,14 +1,26 @@
 package nikita.config;
 
 /**
- * Constants used for ElasticSearch queries
+ * Constants used for OData queries
  */
-public class ESConstants {
+public class ODataConstants {
 
-    public static final String QUERY_SIZE = "size";
-    public static final String QUERY_FROM = "from";
-    public static final String QUERY_QUERY = "query";
-    public static final String QUERY_PREFIX = "prefix";
-    public static final String QUERY_MATCH_PHRASE = "match_phrase";
+    public static final String ODATA_EQ = "eq";
+    public static final String ODATA_GT = "gt";
+    public static final String ODATA_LT = "lt";
+    public static final String ODATA_GE = "ge";
+    public static final String ODATA_LE = "le";
+
+    public static final String SQL_EQ = "=";
+    public static final String SQL_GT = ">";
+    public static final String SQL_LT = "<";
+    public static final String SQL_GE = ">=";
+    public static final String SQL_LE = "<=";
+
+    public static final String HQL_EQ = "=";
+    public static final String HQL_GT = ">";
+    public static final String HQL_LT = "<";
+    public static final String HQL_GE = ">=";
+    public static final String HQL_LE = "<=";
 
 }
