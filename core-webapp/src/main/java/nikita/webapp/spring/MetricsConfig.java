8133b29e51fcf3def1ecc5457cdda9532bca6e4a
Thomas Sødring <tsodring@gmail.com>
2018-03-17 12:22:10 +0100

    Make metrics configurable as a profile
    The reason to do this is to avoid noise in the console as we continue
    development. So this might be temporary.

--- core-webapp/src/main/java/nikita/webapp/spring/MetricsConfig.java
+++ core-webapp/src/main/java/nikita/webapp/spring/MetricsConfig.java
@@ -5,9 +5,13 @@ import com.codahale.metrics.MetricRegistry;
 import com.ryantenney.metrics.spring.config.annotation.EnableMetrics;
 import com.ryantenney.metrics.spring.config.annotation.MetricsConfigurerAdapter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.util.concurrent.TimeUnit;
 
+import static nikita.common.config.Constants.SPRING_PROFILE_METRICS;
+
+@Profile(SPRING_PROFILE_METRICS)
 @Configuration
 @EnableMetrics
 public class MetricsConfig extends MetricsConfigurerAdapter {
