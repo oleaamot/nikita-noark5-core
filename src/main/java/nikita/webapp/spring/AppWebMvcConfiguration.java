85eea3bb3e4ec4d7193ff5f7bda0d3e8b16cc0be
Thomas Sødring <tsodring@gmail.com>
2018-03-23 17:51:25 +0100

    Remove unused code.
    As part of general cleanup of code, we are removing code that is
    unnecessary. This typically hung around from early implementation.

--- src/main/java/nikita/webapp/spring/AppWebMvcConfiguration.java
+++ src/main/java/nikita/webapp/spring/AppWebMvcConfiguration.java
@@ -3,7 +3,6 @@ package nikita.webapp.spring;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import nikita.webapp.web.interceptor.NikitaETAGInterceptor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.http.CacheControl;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -11,17 +10,13 @@ import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 import java.util.List;
-import java.util.Locale;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
@@ -77,24 +72,6 @@ public class AppWebMvcConfiguration implements WebMvcConfigurer {
     }
 
     /**
-     *
-     * @param configurer
-     *
-     * Explicitly set default return type for requests to be application/json
-     *
-     * For some reason the application is setting the default return type to XML, not JSON
-     * so this method forces the default to be JSON.
-     */
-    /*
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.defaultContentType(MediaType.APPLICATION_JSON_UTF8);
-        // Just here as it might be needed when we consume content
-        //configurer.mediaType("application/vnd.noark5-v4+json;charset=UTF-8", MediaType.APPLICATION_JSON_UTF8);
-        //configurer.mediaType("application/vnd.noark5-v4+json", MediaType.APPLICATION_JSON);
-    }
-*/
-    /**
       *  Needed to serve the UI-part of swagger
       */
     @Override
@@ -113,41 +90,8 @@ public class AppWebMvcConfiguration implements WebMvcConfigurer {
                 .addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"));
     }
 
-    /**
-     * Set the LocaleResolver for the GUI portion of the application
-     * By default english pages with be returned to the user
-     * @return LocaleResolver
-     */
-    @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.US);
-        return slr;
-    }
-
-    /**
-     *  Set interceptor so that GUI portion of the applications
-     *  can return pages with different languages. Take a look
-     *  for a files called message_LANG.properties under the
-     *  /src/main/resources/ folder to see which languages are
-     *  supported at runtime.
-     *
-     *  TODO: Check how this deals with accept-language in HTTP
-     *  header
-     *  lang=no, lang=en
-     * @return LocaleChangeInterceptor
-     */
-
-    @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-        lci.setParamName("lang");
-        return lci;
-    }
-
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
         registry.addInterceptor(new NikitaETAGInterceptor());
     }
 
