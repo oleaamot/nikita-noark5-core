c8e69f18c18a54922569a855deadcfefd14e567b
Ole Aamot <oka@oka.no>
2018-03-25 22:07:30 +0200

    Update query string in Search GUI

--- web/dependencies/internal/sok.js
+++ web/dependencies/internal/sok.js
@@ -14,11 +14,7 @@ app.controller('SearchController', ['$scope', '$http', function ($scope, $http) 
 	console.log(" The value of element is " + elementValue);
 	console.log(" The value of filter is " + filterValue);
 	console.log(" The value of string is " + stringValue);
-	// Shows how to get value
-	var urlToFonds = base_url + "odata/arkivstruktur/arkiv?%24filter%3Dcontains%28tittel%2C%27" + stringValue + "%27%29%24top%3D2%24skip%3D0%24orderby%3Dtittel%20desc";
-	// noarkObject should get a value from the html page
-	var noarkObject = 'arkiv';
-	urlToFonds = base_url + "odata/arkivstruktur/" + noarkObject + "?%24filter%3Dcontains%28tittel%2C%27%27%29%24top%3D2%24skip%3D0%24orderby%3Dtittel%20desc";
+	urlToFonds = base_url + "odata/arkivstruktur/" + elementValue + "?%24filter%3Dcontains%28" + contentValue + "%2C%27" + stringValue + "%27%29%24top%3D2%24skip%3D0%24orderby%3D" + contentValue + "%20desc";
 	console.log("Attempting search on " + urlToFonds);
 	var token = GetUserToken();
 	$http({
